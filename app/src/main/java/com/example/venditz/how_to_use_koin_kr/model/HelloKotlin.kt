aa4a2d45338434a0ebefa4df07b6b83babfe47c8
Oh,Seunghoon <stdsalmon@gmail.com>
2018-11-22 10:58:02 +0900

    init proj

---
+++ app/src/main/java/com/example/venditz/how_to_use_koin_kr/model/HelloKotlin.kt
@@ -0,0 +1,2 @@
+package com.example.venditz.how_to_use_koin_kr.model
+
---
+++ app/src/main/java/com/example/venditz/how_to_use_koin_kr/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.venditz.how_to_use_koin_kr.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.venditz.how_to_use_koin_kr.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
